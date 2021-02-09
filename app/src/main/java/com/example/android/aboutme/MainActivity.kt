0f9f3709921dee88e85a0fd08a656be71a730651
Adrian <ajim1989@gmail.com>
2021-02-09 15:35:28 -0800

    Added data for data binding

--- app/src/main/java/com/example/android/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/android/aboutme/MainActivity.kt
@@ -15,10 +15,14 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val myName: MyName = MyName("First Last")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
 
         /*findViewById<Button>(R.id.done_button).setOnClickListener {
             addNickname(it)
@@ -38,7 +42,8 @@ class MainActivity : AppCompatActivity() {
         nicknameTextView.visibility = View.VISIBLE*/
 
         binding.apply {
-            nicknameText.text = nicknameEdit.text
+            //nicknameText.text = nicknameEdit.text
+            myName?.nickname = nicknameEdit.text.toString()
             invalidateAll()
             nicknameEdit.visibility = View.GONE
             view.visibility = View.GONE
